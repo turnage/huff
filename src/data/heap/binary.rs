4b530d6a58a6131a9e50ccbdd66d2d968ace3692
Payton Turnage <paytonturnage@gmail.com>
2017-03-24 14:01:34 -0400

    Consolidate the KV pair into one generic.
    A KV pair struct can implement Ord; this reduces the number of razor blades on
    my eyes in this source code.

--- src/data/heap/binary.rs
+++ src/data/heap/binary.rs
@@ -2,27 +2,18 @@
 
 use std::usize;
 
-use data::Elem;
-use data::heap::{Heap, Node, HeapTest};
+use data::heap::{Heap, HeapTest};
 
 /// BinaryHeap maintains a binary tree...
-///   1. whose root is the maximum element, and among the elements in the tree of equal value, it is
-///      the first inserted.
-///   2. which is complete.
-pub struct BinaryHeap<K: Ord, V> {
-    state: Vec<Node<K, V>>,
-
-    // order decrements after every insertion, infinitely, and the value at insertion is assigned
-    // to the inserted element. This maintains an order for elements whose keys have equal value.
-    order: usize,
+///   - whose root is the maximum element.
+///   - which is complete.
+pub struct BinaryHeap<T: Ord> {
+    state: Vec<T>,
 }
 
-impl<K: Ord, V> BinaryHeap<K, V> {
+impl<T: Ord> BinaryHeap<T> {
     pub fn max() -> Self {
-        BinaryHeap {
-            state: Vec::new(),
-            order: usize::MAX,
-        }
+        BinaryHeap { state: Vec::new() }
     }
 
     fn inconsistent_child(&self, i: usize) -> Option<usize> {
@@ -46,7 +37,7 @@ impl<K: Ord, V> BinaryHeap<K, V> {
         }
     }
 
-    fn node(&self, i: usize) -> Option<&Node<K, V>> {
+    fn node(&self, i: usize) -> Option<&T> {
         if self.exists(i) {
             Some(&self.state[i])
         } else {
@@ -55,7 +46,7 @@ impl<K: Ord, V> BinaryHeap<K, V> {
     }
 
     fn lt_parent(&self, i: usize) -> bool {
-        self.is_root(i) || self.state[i].elem.key <= self.state[self.parent(i)].elem.key
+        self.is_root(i) || self.state[i] <= self.state[self.parent(i)]
     }
 
     fn is_root(&self, i: usize) -> bool {
@@ -79,13 +70,9 @@ impl<K: Ord, V> BinaryHeap<K, V> {
     }
 }
 
-impl<K: Ord, V> Heap<K, V> for BinaryHeap<K, V> {
-    fn insert(&mut self, k: K, v: V) {
-        self.state.push(Node {
-            elem: Elem { key: k, val: v },
-            order: self.order,
-        });
-        self.order -= 1;
+impl<T: Ord> Heap<T> for BinaryHeap<T> {
+    fn insert(&mut self, e: T) {
+        self.state.push(e);
         let mut n = self.state.len() - 1;
         while !self.is_root(n) && !self.lt_parent(n) {
             let parent = self.parent(n);
@@ -94,22 +81,26 @@ impl<K: Ord, V> Heap<K, V> for BinaryHeap<K, V> {
         }
     }
 
-    fn pop(&mut self) -> Option<V> {
-        if self.state.len() == 0 {
+    fn peek(&self) -> Option<&T> {
+        self.state.first()
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        if self.state.is_empty() {
             None
         } else {
             let mut n = 0;
-            let v = self.state.swap_remove(n);
+            let e = self.state.swap_remove(n);
             while let Some(c) = self.inconsistent_child(n) {
                 self.state.swap(n, c);
                 n = c
             }
-            Some(v.elem.val)
+            Some(e)
         }
     }
 }
 
-impl<K: Ord, V> HeapTest<K, V> for BinaryHeap<K, V> {
+impl<T: Ord> HeapTest<T> for BinaryHeap<T> {
     fn validate_node(&self, n: usize) {
         if !self.exists(n) {
             return;
--- src/data/heap/mod.rs
+++ src/data/heap/mod.rs
@@ -4,7 +4,6 @@ pub mod binary;
 
 use std::cmp::Ordering;
 use std::fmt::Display;
-use data::Elem;
 
 /// Heap holds key value pairs and always pops the element with the highest value key which was
 /// inserted first.
@@ -12,19 +11,20 @@ use data::Elem;
 /// Worst cases
 ///     Insertion O(log(n))
 ///     Pop O(log(n))
-pub trait Heap<K: Ord, V> {
-    fn insert(&mut self, k: K, v: V);
-    fn pop(&mut self) -> Option<V>;
+pub trait Heap<T: Ord> {
+    fn insert(&mut self, e: T);
+    fn peek(&self) -> Option<&T>;
+    fn pop(&mut self) -> Option<T>;
 }
 
-impl<K: Ord, V> Iterator for Heap<K, V> {
-    type Item = V;
+impl<T: Ord> Iterator for Heap<T> {
+    type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         self.pop()
     }
 }
 
-trait HeapTest<K: Ord, V>: Heap<K, V> {
+trait HeapTest<T: Ord>: Heap<T> {
     fn validate(&self) {
         self.validate_node(0);
     }
@@ -32,79 +32,25 @@ trait HeapTest<K: Ord, V>: Heap<K, V> {
     fn validate_node(&self, n: usize);
 }
 
-struct Node<K: Ord, V> {
-    elem: Elem<K, V>,
-    order: usize,
-}
-
-impl<K: Ord, V> Ord for Node<K, V> {
-    fn cmp(&self, other: &Node<K, V>) -> Ordering {
-        match self.elem.cmp(&other.elem) {
-            Ordering::Equal => self.order.cmp(&other.order),
-            o => o,
-        }
-    }
-}
-
-impl<K: Ord, V> PartialOrd for Node<K, V> {
-    fn partial_cmp(&self, other: &Node<K, V>) -> Option<Ordering> {
-        Some(self.cmp(&other))
-    }
-}
-
-impl<K: Ord, V> Eq for Node<K, V> {}
-
-impl<K: Ord, V> PartialEq for Node<K, V> {
-    fn eq(&self, other: &Node<K, V>) -> bool {
-        self.elem == other.elem
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
 
-    fn input() -> Vec<Node<usize, usize>> {
-        vec![Node {
-                 elem: Elem { key: 1, val: 4 },
-                 order: 0,
-             },
-             Node {
-                 elem: Elem { key: 1, val: 5 },
-                 order: 1,
-             },
-             Node {
-                 elem: Elem { key: 3, val: 1 },
-                 order: 2,
-             },
-             Node {
-                 elem: Elem { key: 4, val: 0 },
-                 order: 3,
-             },
-             Node {
-                 elem: Elem { key: 2, val: 2 },
-                 order: 4,
-             },
-             Node {
-                 elem: Elem { key: 2, val: 3 },
-                 order: 5,
-             }]
+    /// input returns a set of unique elements.
+    fn input() -> Vec<usize> {
+        vec![1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
 
-    fn test_heap<K, V, H>(mut h: H, ns: Vec<Node<K, V>>)
-        where K: Ord + Copy + Display,
-              V: Ord + Copy + Display,
-              H: Heap<K, V> + HeapTest<K, V>
-    {
+    fn test_heap<T: Ord + Copy + Display, H: Heap<T> + HeapTest<T>>(mut h: H, ns: Vec<T>) {
         for n in ns.iter() {
-            h.insert(n.elem.key, n.elem.val);
+            h.insert(*n);
         }
         h.validate();
 
         let mut extracted = 0;
-        let mut last_value = ns.iter().map(|n| n.elem.val).min().unwrap();
+        let mut last_value = *ns.iter().max().unwrap();
         while let Some(v) = h.pop() {
-            assert!(v >= last_value);
+            assert!(v <= last_value);
             last_value = v;
             extracted += 1;
             h.validate();
